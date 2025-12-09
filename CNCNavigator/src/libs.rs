ecc4c297c5cf24e7305e70c18d411695caf7740b
Bruno Ferreira <fm.brunoferreira@gmail.com>
2025-12-09 16:47:58 +0000

    Init

---
+++ CNCNavigator/src/libs.rs
@@ -0,0 +1,42 @@
+pub use iced::{
+    application,
+    mouse,
+    Event,
+    Alignment,
+    Color,
+    Length,
+    Point,
+    Rectangle,
+    Renderer,
+    Size,
+    Subscription,
+    Settings,
+    Theme,
+    Background,
+    theme,
+    Border,
+    window,
+    Padding
+};
+
+pub use iced::widget::{
+    button,
+    column,
+    container,
+    row,
+    slider,
+    text
+};
+
+pub use iced::widget::canvas::{
+    self,
+    Canvas,
+    Frame,
+    Geometry,
+    Path,
+    Stroke
+};
+
+pub use iced::alignment::{
+    Horizontal
+};
---
+++ CNCNavigator/src/main.rs
@@ -0,0 +1,16 @@
+mod libs;
+mod components;
+mod enums;
+mod interfaces;
+mod styles;
+
+use libs::*;
+use crate::components::*;
+use crate::enums::*;
+use crate::interfaces::*;
+use crate::styles::*;
+
+
+fn main() {
+    println!("Hello, world!");
+}
